b5c108a9f44ea2a5afaa8d2b042809980061ec17
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-10-12 14:56:36 +0600

    home page html added

--- js/login.js
+++ js/login.js
@@ -1,10 +1,31 @@
 // console.log('login.js')
 
+// document
+//   .getElementById("login-btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     const pinCode = document.getElementById("pin-code").value;
+//     console.log(phoneNumber, pinCode);
+//     if(phoneNumber === '01577065084' && pinCode ==='inaba999'){
+//         console.log('you are logged in');
+//     }
+//     else {
+//         alert('wrong password or email')
+//     }
+//   });
+
 document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("log in button clicked");
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinCode = document.getElementById("pin-code").value;
+    console.log(phoneNumber, pinCode);
+    if (phoneNumber === "01577065084" && pinCode == "inaba999") {
+      console.log("You are logged in ");
+      window.location.href = '/home.html';
+    } else {
+      alert("wrong phone number or password");
+    }
   });
